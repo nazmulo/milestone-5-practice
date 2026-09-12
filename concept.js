e83534a6f7caeadf4e4baab9427203919cd09507
nazmulo <itsnaazmul@gmail.com>
2026-09-12 12:29:57 +0600

    default perameter

---
+++ concept.js
@@ -0,0 +1,16 @@
+//default parameter:
+
+//function add (num1, num2 = 0){
+//    const total = num1 + num2;
+//    console.log(num1,num2,total);
+//}
+//add(10)
+
+//2 string :
+
+//function fullName (first, last = ' '){
+//    const name = first + ' ' + last;
+//    console.log(name);
+//}
+//
+//fullName('kamal')
