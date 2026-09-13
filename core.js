539b03e598c60a45b47b44fd711b6ebd46662de5
nazmulo <itsnaazmul@gmail.com>
2026-09-13 17:13:39 +0600

    nul and undefine

--- core.js
+++ core.js
@@ -1,10 +1,41 @@
 // primitive and not primitive data type:
 
 //primitive:
-let a = 4;
-let b = 'Tormuj';
-let c = false;
+//let a = 4;
+//let b = 'Tormuj';
+//let c = false;
 
 //non-primitive:
-let d = {price:22};
-let e = [1,2,3,4,5];
+//let d = {price:22};
+//let e = [1,2,3,4,5];
+
+//null and undefine:
+
+//let data;
+//console.log(data);
+
+//const sum = (a,b) =>{
+//    console.log(a,b);
+//};
+//sum(5);
+
+//const student = {
+//    name: 'Tonmoy',
+//    age: 22,
+//};
+
+//console.log(student.age);
+
+//console.log(student.marks);
+
+//delete student.name;
+
+//console.log(student.name);
+
+//const arr =  [1,2,3,4];
+//console.log(arr[2]);
+//console.log(arr[5]);
+//delete arr[1];
+//console.log(arr);
+//console.log(arr[1]);
+
