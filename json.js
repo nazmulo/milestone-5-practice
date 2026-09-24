689572c76f3711bdaac5a6be63b048efdb6a16eb
nazmulo <itsnaazmul@gmail.com>
2026-09-24 14:39:53 +0600

    disploay post

--- json.js
+++ json.js
@@ -36,3 +36,13 @@
 //fetch("https://jsonplaceholder.typicode.com/todos/1")
 //    .then((res) => res.json())
 //    .then((data) => console.log(data));
+
+const loadPost = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((res) => res.json())
+    .then((json) => displayPost(json))
+};
+
+const displayPost = (post) => {
+    console.log(post)
+};
